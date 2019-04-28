10309eaba04261cedfe137b0894375063e155fc7
jaswanth alahari <jassuRijju@jaswanths-mbp.home>
2019-04-28 17:11:48 -0400

    JSON Parsing and Laoding the Table View with Parsed data

--- GM_Demo/GM_Demo/ViewController.swift
+++ GM_Demo/GM_Demo/ViewController.swift
@@ -14,20 +14,48 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var tableView: UITableView!
     let url = "https://api.github.com/repos/jassu1226/github_GMDemo/commits"
     var commitsDataSource = [Commit]()
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getCommitDetails()
+        
         tableView.delegate = self
         tableView.dataSource = self
         
     }
     
-    func getCommitDetails() -> [Commit] {
-        var commits = [Commit]()
-        return commits
+    //MARK: - Networking
+    func getCommitDetails()  {
+        self.commitsDataSource.removeAll()
+        
+        Alamofire.request(url, method: .get).responseJSON { (response) in
+            if response.result.isSuccess {
+                let commitResponseJSON: JSON = JSON(response.result.value ?? "Network Issues")
+                for  commitValues in commitResponseJSON.array!  {
+                    
+                    let tempCommit = Commit()
+                    tempCommit.commitName = commitValues["commit"]["author"]["name"].string!
+                    tempCommit.CommitMessage = commitValues["commit"]["message"].string!
+                    tempCommit.commitId = commitValues["commit"]["tree"]["sha"].string!
+                    
+                    self.commitsDataSource.append(tempCommit)
+                }
+            }else {
+                print("Error: \(String(describing: response.result.error))")
+                
+            }
+            self.tableView.reloadData()
+        }
+        
+        
     }
+    
+    //Mark: JSON Parsing
+    
+    
+    
+    //MARK: - TableView DataSource Methods
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return commitsDataSource.count
@@ -36,9 +64,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CommitDetails", for: indexPath) as! CommitDetailCell
         
+        cell.commiterName.text = commitsDataSource[indexPath.row].commitName
+        cell.commitMsg.text = commitsDataSource[indexPath.row].CommitMessage
+        cell.commitId.text = commitsDataSource[indexPath.row].commitId
+        
         return cell
     }
-
+    
+    // MARK: - Tableview delegate Methods
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 
 }
 
